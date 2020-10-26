c389515125f0beb4fa966925489c715090ac7072
leahgrace555 <leah.grace555@gmail.com>
2020-10-26 19:02:23 -0400

    connected to mongo databse

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const server = require('./server.js');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT;
-//mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 server.listen(PORT || 3001, ()=> {
   console.log(`server running on port ${PORT}`)
